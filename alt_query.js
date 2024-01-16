a3ce3a6276b8d279d79ebe3f0ebea031d998a61c
julianeon <julianeon@gmail.com>
2024-01-16 11:48:32 -0800

    better formatting

--- alt_query.js
+++ alt_query.js
@@ -1,17 +1,17 @@
 const graphqlQuery = {
   query: `
-{
-  transfers(where: { blockNumber: 16940391 }) {
-    id
-    from
-    to
-    value
-    blockNumber
-    blockTimestamp
-    transactionHash
+  {
+    transfers(where: { blockNumber: 16940391 }) {
+      id
+      from
+      to
+      value
+      blockNumber
+      blockTimestamp
+      transactionHash
+    }
   }
-}
-  `
+`
 };
 
 module.exports = graphqlQuery;
